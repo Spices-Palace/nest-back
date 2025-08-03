3c45789f46d47fdff429e4b47ac1426df9acf881
fasil9955 <fasilpvr52@gmail.com>
2025-08-03 15:40:16 +0530

    addeddddd

--- billing-system/src/v1/bill/bill.controller.ts
+++ billing-system/src/v1/bill/bill.controller.ts
@@ -94,7 +94,7 @@ export class BillController {
     }
     
     // Calculate total sales from payment breakdown
-    totalSales = paymentBreakdown.cash + paymentBreakdown.gpay + paymentBreakdown.card ;
+    totalSales = paymentBreakdown.cash + paymentBreakdown.gpay + paymentBreakdown.card + paymentBreakdown.other;
     
     return { 
       totalSales, 
